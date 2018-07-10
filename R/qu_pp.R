997810568179b27c355e26ae853c86cf4061a06d
andreasLD <andschar@gmail.com>
2018-07-10 16:29:26 +0200

    added PHYSPROP database

---
+++ R/qu_pp.R
@@ -0,0 +1,53 @@
+# script to query information from the SRC PHYSPROP Database
+# https://www.srcinc.com/what-we-do/environmental/scientific-databases.html
+
+# setup -------------------------------------------------------------------
+source('R/setup.R')
+# switches
+online = online
+# online = TRUE
+
+# data --------------------------------------------------------------------
+psm = readRDS(file.path(cachedir, 'psm.rds'))
+
+# query -------------------------------------------------------------------
+todo_pp = psm$cas
+# todo_pp = todo_pp[1:10] # debug me!
+
+if (online) {
+  pp_l = pp_query(todo_pp)
+  
+  saveRDS(pp_l, file.path(cachedir, 'pp_l.rds'))
+} else {
+  pp_l = readRDS(file.path(cachedir, 'pp_l.rds'))
+}
+
+# convenience function
+pp_resolve = function(l) {
+  
+  # l = pp_l[[4]] # debug me!
+  dt = data.table::dcast(l$prop, . ~ variable, value.var = 'value')
+  dt$`.` = NULL
+  dt$cname = l$cname
+  dt$mw = l$mw
+  dt$source_url = l$source_url
+
+  return(dt)
+}
+
+pp_l[!is.na(pp_l)] = lapply(pp_l[!is.na(pp_l)], pp_resolve)
+pp_l[is.na(pp_l)] = lapply(pp_l[is.na(pp_l)], data.table)
+
+pp = rbindlist(pp_l, fill = TRUE, idcol = 'cas')
+pp[ , V1 := NULL ]
+setcolorder(pp, c('cas', 'cname'))
+
+# cleaning ----------------------------------------------------------------
+rm(list = ls()[!ls() %in% c('pp_l', 'pp')])
+
+
+
+
+
+
+
