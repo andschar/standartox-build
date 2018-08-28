d83f9500d94680b5bf08479940f09aa602d64d07
andreasLD <andschar@gmail.com>
2018-08-28 16:21:28 +0200

    phantomjs web scrape function

---
+++ R/fun_scrape_phantomjs.R
@@ -0,0 +1,54 @@
+# function to scrape web sites that load their content via JS functions
+# http://www.rladiesnyc.org/post/scraping-javascript-websites-in-r/
+# PhantomJS needs to be installed!
+
+# setup -------------------------------------------------------------------
+require(V8)
+
+
+# Java script -------------------------------------------------------------
+writeLines(
+  text =
+    "var url ='http://www.example.com';
+    var page = new WebPage()
+    var fs = require('fs');
+    
+    
+    page.open(url, function (status) {
+      just_wait();
+    });
+    
+    function just_wait() {
+      setTimeout(function() {
+        fs.write('1.html', page.content, 'w');
+        phantom.exit();
+      }, 2500);
+    }",
+  con = file.path(tempdir(), 'scrape.js')
+)
+
+
+# R -----------------------------------------------------------------------
+js_scrape = function(url = 'http://www.example.com',
+                     js_path = file.path(tempdir(), 'scrape.js'),
+                     phantompath = '/usr/bin/phantomjs',
+                     file = file.path(tempdir(), 'file.html')) {
+  
+  lines = readLines(js_path)
+  lines[1] = paste0("var url = '", url ,"';")
+  lines[12] = paste0("        fs.write('", file, "', page.content, 'w');")
+  writeLines(lines, js_path)
+  
+  command = paste(phantompath, js_path, '/tmp/test.html', sep = " ")
+  system(command)
+  
+  message('Saving to: ', file)
+}
+
+
+
+
+
+
+
+
