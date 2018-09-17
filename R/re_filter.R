dd20748ac47899fb552c57a077097216d963c117
andreasLD <andschar@gmail.com>
2018-09-17 13:34:57 +0200

    out sourced checks

--- R/re_filter.R
+++ R/re_filter.R
@@ -12,43 +12,32 @@ if (online) {
 
 # variables ---------------------------------------------------------------
 
-# (1) compound name ----
-tests[ , comp_name := NA ]
-tests[ , comp_name := ifelse(!is.na(pp_cname), pp_cname,
-                      ifelse(is.na(comp_name) & !is.na(aw_cname), aw_cname,
-                      ifelse(is.na(comp_name) & !is.na(fr_cname), fr_cname,
-                      ifelse(is.na(comp_name) & !is.na(pc_iupacname), pc_iupacname, NA)))) ]
+# CONTINUE HERE!!!
+# TODO add source variable
 
-# checking
-na_name = tests[ is.na(comp_name), .N, by = c('casnr') ][ , N := NULL]
-if (length(na_name$casnr) > 0 ) {
-  message('For the following cas, compound names are missing:\n',
-          paste0(na_name$casnr, collapse = ', '))
-}
+# (1) compound name ----
+cols = c('comp_name', 'comp_name_src')
+tests[ , (cols) := list(pp_cname, 'pp') ]
+tests[ is.na(comp_name), (cols) := list(aw_cname, 'aw') ]
+tests[ is.na(comp_name), (cols) := list(fr_cname, 'fr')  ]
+tests[ is.na(comp_name), (cols) := list(pc_iupacname, 'pc')  ]
+tests[ is.na(comp_name), (cols) := list(ep_chemical_name, 'ep') ]
+tests[ is.na(comp_name), (cols) := NA ] # necessary 'cause string (e.g. 'aw') is recycled nrow times
 
 # (2) compound type ----
-tests[ , comp_type := NA ]
-tests[ , comp_type := ifelse(!is.na(aw_pest_type), aw_pest_type,
-                      ifelse(is.na(comp_type) & !is.na(ep_chemical_group), ep_chemical_group,
-                      ifelse(is.na(comp_type) & !is.na(fr_cname), fr_cname, NA))) ]
-
-na_type = tests[ is.na(comp_type), .N, by = c('casnr') ][ , N := NULL]
-if (length(na_type$casnr) > 0 ) {
-  message('For the following cas, compound types are missing:\n',
-          paste0(na_type$casnr, collapse = ', '))
-}
+cols = c('comp_type', 'comp_type_src')
+tests[ , (cols) := list(aw_pest_type, 'aw') ]
+# TODO put pubchem here
+tests[ , (cols) := list(fr_chemical_group, 'fr') ]
+tests[ , (cols) := list(ep_chemical_group, 'ep') ]
+tests[ is.na(comp_type), (cols) := NA ]
 
 # (3) water solubility ----
-tests[ , comp_solub := ifelse(!is.na(pp_solubility_water), pp_solubility_water, NA)]
+cols = c('comp_solub', 'comp_solub_src')
+tests[ , (cols) := list(pp_solubility_water, 'pp') ]
+tests[ is.na(comp_solub), (cols) := NA ]
 # TODO add more resources to the solub_wat_fin creation
-tests[ , comp_solub_chck := ifelse(ep_value < comp_solub, TRUE, FALSE)] # TODO check: unit of solubility concentrations
-
-# checking
-na_solub = tests[ is.na(comp_solub), .N, by = c('casnr') ][ , N := NULL]
-if (length(na_solub$casnr) > 0 ) {
-  message('For the following cas, water solubility values are missing:\n',
-          paste0(na_solub$casnr, collapse = ', '))
-}
+# TODO check: unit of solubility concentrations
 
 # (4) habitat column ----
 # TODO replace concatenated is.na checks with this: https://stackoverflow.com/questions/42701577/multiple-column-condition-in-data-table
@@ -57,44 +46,19 @@ tests[ , isBra_fin := ifelse(isBra_wo_sp == 1 | isBra_gbif == 1, '1', NA)]
 tests[ , isMar_fin := ifelse(ep_media_type == 'SW' | isMar_wo_sp == 1 | isMar_gbif == 1, '1', NA)]
 tests[ , isTer_fin := ifelse(ep_habitat == 'Soil' | isTer_wo_sp == 1 | isTer_gbif == 1 | isTer_gbif == 1, '1', NA)]
 
-# checking
-na_habi = tests[ is.na(isFre_fin) & is.na(isBra_fin) & is.na(isMar_fin) & is.na(isTer_fin),
-                 .N,
-                 by = c('taxon') ][ , N := NULL]
-if (length(na_habi$taxon) > 0 ) {
-  message('For the following taxa, habitat entries are missing:\n',
-          paste0(na_habi$taxon, collapse = ', '))
-}
-
 # (5) regional column ----
-# checking
-na_regi = tests[ is.na(gb_africa) & is.na(gb_antarctica) & is.na(gb_asia) & is.na(gb_europe) &
-                 is.na(gb_north_america) & is.na(gb_oceania) & is.na(gb_south_america),
-                 .N,
-                 by = c('taxon') ][ , N := NULL]
-if (length(na_regi$taxon) > 0 ) {
-  message('For the following taxa, habitat entries are missing:\n',
-          paste0(na_regi$taxon, collapse = ', '))
-}
+tests[ , is_africa := ifelse(gb_africa == 1, 1, NA) ]
+tests[ , is_america_north := ifelse(gb_north_america == 1, 1, NA) ]
+tests[ , is_america_south := ifelse(gb_south_america == 1, 1, NA) ]
+tests[ , is_asia := ifelse(gb_asia == 1, 1, NA) ]
+tests[ , is_europe := ifelse(gb_europe == 1, 1, NA) ]
+tests[ , is_oceania := ifelse(gb_oceania == 1, 1, NA) ]
 
 # save data ---------------------------------------------------------------
 tests_fl = copy(tests)
 # TODO rm after presentation and change it in the right place
 tests_fl[comp_name == 'glyphosphate', comp_name := 'glyphosate']
 saveRDS(tests_fl, file.path(cachedir, 'tests_fl.rds'))
-
-
-# missing data ------------------------------------------------------------
-missing_l = list(final_na_name = na_name, final_na_type = na_type, final_na_solub = na_solub, final_na_habi = na_habi, final_na_regi = na_regi)
-for (i in 1:length(missing_l)) {
-    file = missing_l[[i]]
-    name = names(missing_l)[i]
-  
-  if (nrow(file) > 0) {
-    fwrite(file, file.path(missingdir, paste0(name, '.csv')))
-    message('Writing file with missing data: ', name)
-  }
-}
 
 # cleaning ----------------------------------------------------------------
 oldw = getOption("warn")
---
+++ R/re_filter_stats.R
@@ -0,0 +1,90 @@
+# script to clean the EPA ECOTOX data base test results
+
+# setup -------------------------------------------------------------------
+source('R/setup.R')
+
+# data --------------------------------------------------------------------
+tests_fl_stat = readRDS(file.path(cachedir, 'tests_fl.rds'))
+rows_cas = nrow(tests_fl_stat[ , unique(.SD), .SDcols = 'cas' ])
+rows_tax = nrow(tests_fl_stat[ , unique(.SD), .SDcols = 'taxon' ])
+
+# functions ---------------------------------------------------------------
+chck_na_cas = function (dt, var) { dt[ is.na(get(var)), unique(.SD), .SDcols = c('cas', var) ] }
+chck_na_tax = function (dt, var) { dt[ is.na(get(var)), unique(.SD), .SDcols = c('taxon', var) ] }
+
+# chemical variables ------------------------------------------------------
+chem_vars = c('cas', 'comp_name', 'comp_type', 'comp_solub')
+na_chem_l = mapply(chck_na_cas, chem_vars, MoreArgs = list(dt = tests_fl_stat), SIMPLIFY = FALSE)
+
+chem_missing = data.table(
+  variable = names(sapply(na_chem_l, nrow)),
+  missing = sapply(na_chem_l, nrow),
+  n = rows_cas
+)
+chem_missing[ , perc := round(missing / n, 2) ]
+
+
+# taxonomic variables -----------------------------------------------------
+taxa_missing = data.table(NA)
+
+
+# habitat variables -------------------------------------------------------
+habitat_vars = c('isMar_fin', 'isBra_fin', 'isFre_fin', 'isTer_fin')
+
+# (1)
+habi = tests_fl_stat[ , lapply(.SD, mean), by = taxon, .SDcols = habitat_vars ]
+habi[ , variable := rowSums(.SD, na.rm = TRUE), .SDcols = habitat_vars ] # column with all combined
+habi_missing1 = habi[ , .(missing = .N), variable ][ variable == 0 ]
+habi_missing1[ , n := rows_tax ][ , perc := round(missing / n,2) ]
+habi_missing1[ , variable := 'is_habi_all' ]
+
+# (2)
+na_habi_l = mapply(chck_na_tax, habitat_vars, MoreArgs = list(dt = tests_fl_stat), SIMPLIFY = FALSE)
+
+habi_missing2 = data.table(
+  variable = names(sapply(na_habi_l, nrow_perc)),
+  missing = sapply(na_habi_l, nrow),
+  n = rows_tax
+)
+habi_missing2[ , perc := round(missing / n, 2) ]
+habi_missing = rbindlist(list(habi_missing1, habi_missing2))
+
+# region variables --------------------------------------------------------
+continent_vars = c('is_africa', 'is_america_north', 'is_america_south', 'is_asia', 'is_europe', 'is_oceania')
+
+# (1)
+cont = tests_fl_stat[ , lapply(.SD, mean), by = taxon, .SDcols = continent_vars ]
+cont[ , variable := rowSums(.SD, na.rm = TRUE), .SDcols = continent_vars ] # column with all combined
+cont_missing1 = cont[ , .(missing = .N), variable ][ variable == 0 ]
+cont_missing1[ , n := rows_tax ][ , perc := round(missing / n,2) ]
+cont_missing1[ , variable := 'is_cont_all' ]
+
+# (2)
+na_cont_l = mapply(chck_na_tax, continent_vars, MoreArgs = list(dt = tests_fl_stat), SIMPLIFY = FALSE)
+
+cont_missing2 = data.table(
+  variable = names(sapply(na_cont_l, nrow)),
+  missing = sapply(na_cont_l, nrow),
+  n = rows
+)
+cont_missing2[ , perc := round(missing / n, 2) ]
+cont_missing = rbindlist(list(cont_missing1, cont_missing2))
+
+# authorized --------------------------------------------------------------
+
+
+
+# write to file -----------------------------------------------------------
+missing_l = list(chem_missing = chem_missing,
+                 taxa_missing = taxa_missing,
+                 habi_missing = habi_missing,
+                 cont_missing = cont_missing)
+
+missing_dt = rbindlist(missing_l, fill = TRUE, idcol = 'group')[ , V1 := NULL ]
+
+fwrite(missing_dt, file.path(cachedir, 'tests_fl_stat.csv'))
+
+
+
+
+
