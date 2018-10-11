30656bd1ce3e439391dee1d40cba251e20d1ab8e
andreasLD <andschar@gmail.com>
2018-10-11 12:21:16 +0200

    added run file

---
+++ R/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Rscript run_build_test.R
+
+# time this script tp run every 2 months
+# to run it in terminal: bash run_build.R
+# crontab:
+# 00 22 * * * /home/scharmueller/Projects/run_build.sh
+
