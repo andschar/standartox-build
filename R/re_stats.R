efd8defefc5a1daf53abe698799526203269486a
andreasLD <andschar@gmail.com>
2018-10-30 14:31:26 +0100

    finished script

--- R/re_stats.R
+++ R/re_stats.R
@@ -1,70 +1,115 @@
-# script to calculate descriptive statistics for filter test results
+# script to determine the numer of NAs for each variable (group) + plots
 
 # setup -------------------------------------------------------------------
 source(file.path(src, 'setup.R'))
-source(file.path(src, 'fun_output_stats.R'))
 
 # data --------------------------------------------------------------------
-te_fl = readRDS(file.path(cachedir, 'tests_fl.rds'))
+# don't read final table 'cause it doesn't contain anymore all variables. Read checked table.
+te_st = readRDS(file.path(cachedir, 'tests_ch.rds'))
+cols = sort(names(te_st))
+cols = grep('src', cols, value = TRUE, invert = TRUE) # remove src columns
 
-# preparation -------------------------------------------------------------
 
-te_stats_l = list(
-  ## long variables ----
-  ep_effect = data.table(
-    nam = out_stats_lng(te_fl, ep_effect)$nam,
-    val = out_stats_lng(te_fl, ep_effect)$ep_effect
-  ),
-  ep_endpoint = data.table(
-    nam = out_stats_lng(te_fl, ep_endpoint)$nam,
-    val = out_stats_lng(te_fl, ep_endpoint)$ep_endpoint
-  ),
-  ep_conc_type = data.table(
-    nam = out_stats_lng(te_fl, ep_conc_type)$nam,
-    val = out_stats_lng(te_fl, ep_conc_type)$ep_conc_type,
-    n = out_stats_lng(te_fl, ep_conc_type)$N
-  ),
-  ## wide variables ----
-  continent = out_stats_wid(
-    te_fl,
-    c('is_america_north', 'is_america_south', 'is_asia', 'is_oceania', 'is_europe', 'is_africa')
-  ),
-  habitat = out_stats_wid(
-    te_fl,
-    c('is_marin', 'is_brack', 'is_fresh', 'is_terre')
-  )
+#### individual variables -------------------------------------------------
+# every variable stands for its own
+
+# media variables ---------------------------------------------------------
+cols_media = grep('med', cols, value = TRUE)
+cols_media = grep('unit', cols_media, value = TRUE, invert = TRUE)
+na_media = te_st[ , sapply(.SD, function(x) length(which(is.na(x)))), .SDcols = cols_media ]
+
+media = data.table(
+  variable = names(na_media),
+  N_NA = na_media,
+  N_tot = nrow(te_st)
 )
+media[ , NA_perc := round(N_NA / N_tot, 2) * 100 ]
+setorder(media, -NA_perc)
 
-# (2) additions -----------------------------------------------------------
+gg_media = ggplot(media, aes(y = NA_perc, x = reorder(variable, -NA_perc))) +
+  geom_point() +
+  labs(y = 'Proportion NA', x = NULL) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-te_stats_l$ep_conc_type[ , nam_long := ifelse(val == 'A', 'Active Ingredient',
-                                       ifelse(val == 'F', 'Formulation',
-                                       ifelse(val == 'T', 'Total (Metals and single elements)',
-                                       ifelse(is.na(val), '',
-                                       ifelse(val == 'U', 'Unionized',
-                                       ifelse(val == 'L', 'Labile',
-                                       ifelse(val == 'D', 'Dissolved', NA))))))) ]
+# compound variables ------------------------------------------------------
+cols_comp = grep('comp', cols, value = TRUE)
+cols_comp = grep('chck', cols_comp, value = TRUE, invert = TRUE) # remove chck_column
 
-te_stats_l$continent[ , nam_long := ifelse(variable == 'is_america_north', 'North America',
-                                    ifelse(variable == 'is_america_south', 'South America',
-                                    ifelse(variable == 'is_europe', 'Europe',
-                                    ifelse(variable == 'is_asia', 'Asia',
-                                    ifelse(variable == 'is_africa', 'Africa',
-                                    ifelse(variable == 'is_oceania', 'Oceania', NA)))))) ]
-te_stats_l$continent[ , nam_long_stat := paste0(nam_long, ' (', N, ')') ]
+na_comp = te_st[ , sapply(.SD, function(x) length(which(is.na(x)))), .SDcols = cols_comp ]
 
-te_stats_l$habitat[ , nam_long := ifelse(variable == 'is_marin', 'marin',
-                                  ifelse(variable == 'is_brack', 'brackish',
-                                  ifelse(variable == 'is_fresh', 'freshwater',
-                                  ifelse(variable == 'is_terre', 'terrestrial', NA)))) ]
-te_stats_l$habitat[ , nam_long_stat := paste0(nam_long, ' (', N, ')') ]
+comp = data.table(
+  variable = names(na_comp),
+  N_NA = na_comp,
+  N_tot = nrow(te_st)
+)
+comp[ , NA_perc := round(N_NA / N_tot, 2) * 100 ]
+setorder(media, -NA_perc)
 
+#### group variables ------------------------------------------------------
+# variables whose NAs should be evaluated together
+# (e.g. hab_fresh, hab_marin, ...)
+# a single NA is not a problem
 
-# write -------------------------------------------------------------------
-# to shinydir
-saveRDS(te_stats_l, file.path(shinydir, 'data', 'te_stats_l.rds'))
+# chemical class ----------------------------------------------------------
+cols_chcl = grep('cgr', cols, value = TRUE)
 
+te_st[ , N_chcl := rowSums(.SD, na.rm = TRUE), .SDcols = cols_chcl ]
+na_chcl = te_st[ , .N, N_chcl ][order(-N)]
 
+chcl = data.table(
+  varaible = 'chemical classes (cgr_)',
+  N_NA = na_chcl[ N_chcl == 0 ]$N,
+  N_tot = nrow(te_st)
+)
+chcl[ , NA_perc := round(N_NA / N_tot, 2) * 100 ]
+setorder(chcl, -NA_perc)
+
+# habitat variables -------------------------------------------------------
+cols_habi = grep('hab_', cols, value = TRUE)
+
+te_st[ , N_habi := rowSums(.SD, na.rm = TRUE), .SDcols = cols_habi ]
+na_habi = te_st[ , .N, N_habi ][order(-N)]
+
+habi = data.table(
+  varaible = 'habitat (hab_)',
+  N_NA = na_habi[ N_habi == 0 ]$N,
+  N_tot = nrow(te_st)
+)
+habi[ , NA_perc := round(N_NA / N_tot, 2) * 100 ]
+setorder(habi, -NA_perc)
+
+# region ------------------------------------------------------------------
+cols_regi = grep('reg_', cols, value = TRUE)
+
+te_st[ , N_regi := rowSums(.SD, na.rm = TRUE), .SDcols = cols_regi ]
+na_regi = te_st[ , .N, N_regi ][order(-N)]
+
+regi = data.table(
+  varaible = 'regitat (reg_)',
+  N_NA = na_regi[ N_regi == 0 ]$N,
+  N_tot = nrow(te_st)
+)
+regi[ , NA_perc := round(N_NA / N_tot, 2) * 100 ]
+setorder(regi, -NA_perc)
+
+# missing list ------------------------------------------------------------
+na_l = list(media = media,
+            comp = comp,
+            chcl = chcl,
+            habi = habi,
+            regi = regi)
+
+na_dt = rbindlist(na_l, idcol = 'varaible_type')
+
+# writing -----------------------------------------------------------------
+fwrite(na_dt, file.path(missingdir, 'all_variables_na.csv'))
+
+# log ---------------------------------------------------------------------
+msg = 'Summary stats on variables calculated and written'
+log_msg(msg); rm(msg)
+
+# cleaning ----------------------------------------------------------------
+rm(cols)
 
 
 
