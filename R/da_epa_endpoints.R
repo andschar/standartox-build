55c55d7a75d96a9bf004964e8f8ca5579393f5df
andreasLD <andschar@gmail.com>
2018-11-01 16:26:35 +0100

    endpoint grouping

--- R/da_epa_endpoints.R
+++ R/da_epa_endpoints.R
@@ -21,10 +21,42 @@ dbDisconnect(con)
 dbUnloadDriver(drv)
 
 # preparation -------------------------------------------------------------
+# remove / and *
 epts[ , endpoint := gsub('/|\\*', '', endpoint) ]
-epts_fin = epts[ , .(N = sum(n)), endpoint][order(-N)]
+# combine endpoints
+epts[ grep('^[A-z]{0,2}10$', endpoint), 
+      endpoint_grp := 'XX10' ]
+epts[ grep('^[A-z]{0,2}50', endpoint),
+      endpoint_grp := 'XX50' ]
+epts[ grep('(?i)NOEC|NOEL', endpoint),
+      endpoint_grp := 'NOEX' ]
+epts[ grep('(?i)LOEC|LOEL', endpoint),
+      endpoint_grp := 'LOEX' ]
+epts[ is.na(endpoint_grp), endpoint_grp := endpoint ]
+
+# reduce ------------------------------------------------------------------
+epts_fin = epts[ , .(N = sum(n)), endpoint ][order(-N)]
+
+epts_fin2 = epts[ , .(N = sum(n)), endpoint_grp ][order(-N)]
 
 # writing -----------------------------------------------------------------
 fwrite(epts_fin, file.path(cachedir, 'epts_fin.csv'))
+fwrite(epts_fin2, file.path(cachedir, 'epts_fin2.csv'))
+
+# help --------------------------------------------------------------------
+## from Elonen Guide p. 52
+# * denotes endpoints whose provided acronym was modified
+# ECxx - Effective Concentration for xx% of tested organisms
+# LCxx - Lethal Concnetration to xx% of test animals
+# ICxx - Inhibition concentration to xx% of organisms
+# LDxx - Lethal dose to xx% of test animals
+
+
+
+
+
+
+
+
 
 
