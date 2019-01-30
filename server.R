48953368fb1dfda1e700e8ccd9e8cfa66599042c
andreasLD <andschar@gmail.com>
2019-01-30 11:20:47 +0100

    reformated code

--- server.R
+++ server.R
@@ -5,21 +5,19 @@ src = file.path(getwd(), 'R')
 source(file.path(src, 'setup.R'))
 
 server = function(input, output) {
-  
   # (1) preparation ---------------------------------------------------------
   # read csv + action button ------------------------------------------------
   # https://stackoverflow.com/questions/49344468/resetting-fileinput-in-shiny-app
-  rv = reactiveValues(
-    data = NULL,
-    reset = FALSE
-  )
+  rv = reactiveValues(data = NULL,
+                      reset = FALSE)
   observe({
     req(input$file_cas)
     req(!rv$reset)
     
     data = read.csv(input$file_cas$datapath,
-                    header = FALSE, stringsAsFactors = FALSE) # $datapath not very intuitive
-    data = data[ ,1]
+                    header = FALSE,
+                    stringsAsFactors = FALSE) # $datapath not very intuitive
+    data = data[, 1]
     rv$data = data
   })
   observeEvent(input$reset, {
@@ -30,70 +28,85 @@ server = function(input, output) {
   
   # data + reactivity function ----------------------------------------------
   data_fil = reactive({
-    fun_filter(dt = dat,
-               tax = input$tax,
-               habitat = input$habitat,
-               continent = input$continent,
-               conc_type = input$conc_type,
-               effect = input$effect,
-               endpoint = input$endpoint,
-               chem_class = input$chem_class,
-               duration = c(input$dur1, input$dur2),
-               chck_solub = input$chck_solub,
-               cas = rv$data)
+    fun_filter(
+      dt = dat,
+      tax = input$tax,
+      habitat = input$habitat,
+      continent = input$continent,
+      conc_type = input$conc_type,
+      effect = input$effect,
+      endpoint = input$endpoint,
+      chem_class = input$chem_class,
+      duration = c(input$dur1, input$dur2),
+      chck_solub = input$chck_solub,
+      cas = rv$data
+    )
   })
   
   data_agg = reactive({
-    fun_aggregate(dt = fun_filter(dt = dat,
-                                  tax = input$tax,
-                                  habitat = input$habitat,
-                                  continent = input$continent,
-                                  conc_type = input$conc_type,
-                                  effect = input$effect,
-                                  endpoint = input$endpoint,
-                                  chem_class = input$chem_class,
-                                  duration = c(input$dur1, input$dur2),
-                                  chck_solub = input$chck_solub,
-                                  cas = rv$data),
-                  agg = input$agg,
-                  info = input$infocols,
-                  comp = input$comp,
-                  chck_outlier = input$chck_outlier)
+    fun_aggregate(
+      dt = fun_filter(
+        dt = dat,
+        tax = input$tax,
+        habitat = input$habitat,
+        continent = input$continent,
+        conc_type = input$conc_type,
+        effect = input$effect,
+        endpoint = input$endpoint,
+        chem_class = input$chem_class,
+        duration = c(input$dur1, input$dur2),
+        chck_solub = input$chck_solub,
+        cas = rv$data
+      ),
+      agg = input$agg,
+      info = input$infocols,
+      comp = input$comp,
+      chck_outlier = input$chck_outlier
+    )
   })
   
   # (2) output --------------------------------------------------------------
-  output$dat = DT::renderDataTable({data_agg()},
-                                   options = list(
-                                     columnDefs = list(list(
-                                       # targets = 0:4,
-                                       render = JS(
-                                         "function(data, type, row, meta) {",
-                                         "return type === 'display' && data.length > 6 ?",
-                                         "'<span title=\"' + data + '\">' + data.substr(0, 6) + '...</span>' : data;",
-                                         "}"))
-                                     )),
-                                   #dom = 't',
-                                   rownames = FALSE,
-                                   callback = JS('table.page(3).draw(false);'))
+  output$dat = DT::renderDataTable({
+    data_agg()
+  },
+  options = list(columnDefs = list(list(
+    # targets = 0:4,
+    render = JS(
+      "function(data, type, row, meta) {",
+      "return type === 'display' && data.length > 6 ?",
+      "'<span title=\"' + data + '\">' + data.substr(0, 6) + '...</span>' : data;",
+      "}"
+    )
+  ))),
+  #dom = 't',
+  rownames = FALSE,
+  callback = JS('table.page(3).draw(false);'))
   
   
   # plot --------------------------------------------------------------------
-  #pl = 
+  #pl =
   # n_pl = length(pl$x$data)
   # write_feather(n_pl, file.path(cachedir, 'n_pl'))
   
-  output$plotly_sensitivity = renderPlotly({ filagg_pl(data_agg(),
-                                                       plot_type = 'dynamic',
-                                                       xaxis = input$xaxis,
-                                                       yaxis = input$yaxis,
-                                                       cutoff = input$cutoff) })
+  output$plotly_sensitivity = renderPlotly({
+    filagg_pl(
+      data_agg(),
+      plot_type = 'dynamic',
+      xaxis = input$xaxis,
+      yaxis = input$yaxis,
+      cutoff = input$cutoff
+    )
+  })
   output$npl = renderText('3')
   # download ----------------------------------------------------------------
   # https://stackoverflow.com/questions/44504759/shiny-r-download-the-result-of-a-table
   output$download_fil = downloadHandler(
-    filename = function() { 
-      paste(input$tax, #input$habitat, input$continent,
-            paste0(input$dur1, input$dur2), 'data_fil.csv', sep = '_')
+    filename = function() {
+      paste(input$tax,
+            #input$habitat, input$continent,
+            paste0(input$dur1, input$dur2),
+            'data_fil.csv',
+            sep = '_')
     },
     content = function(fname) {
       write.csv(data_fil(), fname, row.names = FALSE)
@@ -101,9 +114,12 @@ server = function(input, output) {
   )
   
   output$download_agg = downloadHandler(
-    filename = function() { 
-      paste(input$tax, #input$habitat, input$continent,
-            paste0(input$dur1, input$dur2), 'data_agg.csv', sep = '_')
+    filename = function() {
+      paste(input$tax,
+            #input$habitat, input$continent,
+            paste0(input$dur1, input$dur2),
+            'data_agg.csv',
+            sep = '_')
     },
     content = function(fname) {
       write.csv(data_agg(), fname, row.names = FALSE)
--- ui.R
+++ ui.R
@@ -32,67 +32,138 @@ sidebar = dashboardSidebar(
   sidebarMenu(
     'Filters',
     id = 'sidebar_filter',
-    menuItem('Compound', tabName = 'compound',
-             splitLayout(
-               fileInput(inputId = 'file_cas', label = 'Upload CAS',
-                         accept = '.csv', placeholder = 'one column .csv'),
-               actionButton(inputId = 'reset', label = 'Reset', style = 'margin-top:37px')
-             ),
-             prettyCheckboxGroup(inputId = 'conc_type', label = 'Concentration type',
-                                 choiceValues = te_stats_l$tes_conc_type$variable,
-                                 choiceNames = te_stats_l$tes_conc_type$nam_fin,
-                                 selected = c('A')),
-             prettyCheckboxGroup(inputId = 'chem_class', label = 'Chemical class',
-                                 choiceValues = te_stats_l$chem_class$variable,
-                                 choiceNames = te_stats_l$chem_class$nam_fin,
-                                 selected = 'cgr_herbicide')
-             ),
-    menuItem('Taxon', tabName = 'taxon',
-             selectInput(inputId = 'tax', label = 'Choose a taxon',
-                         choices = c('Daphniidae', 'Chironomidae', 'Insecta', 'Crustacea', 'Annelida', 'Platyhelminthes', 'Mollusca', 'Makro_Inv', 'Fish', 'Algae', 'Bacillariophyceae', 'Plants')),
-             prettyCheckboxGroup(inputId = 'habitat', label = 'Organism hatbitat',
-                                 choiceValues = te_stats_l$habitat$variable,
-                                 choiceNames = te_stats_l$habitat$nam_fin,
-                                 selected = 'hab_fresh'),
-             prettyCheckboxGroup(inputId = 'continent', label = 'Continent',
-                                 choiceValues = te_stats_l$continent$variable,
-                                 choiceNames = te_stats_l$continent$nam_fin,
-                                 selected = 'reg_europe')
-             ),
-    menuItem('Test', tabName = 'test',
-             splitLayout(
-               numericInput(inputId = 'dur1', label = 'Test durations from', value = 24),
-               numericInput(inputId = 'dur2', label = 'to (h)', value = 48)
-             ),
-             # dateRangeInput(inputId = 'duration', label = 'Duration range (h)',
-             #                value = c(24, 48), format = '%h'),
-             splitLayout(
-               prettyCheckboxGroup(inputId = 'effect', label = 'Effect group',
-                                   choiceValues = te_stats_l$tes_effect$variable,
-                                   choiceNames = te_stats_l$tes_effect$nam_fin,
-                                   selected = c('MOR', 'POP', 'GRO', 'ITX')),
-               prettyCheckboxGroup(inputId = 'endpoint', label = 'Endpoints',
-                                   choiceValues = te_stats_l$tes_endpoint$variable,
-                                   choiceNames = te_stats_l$tes_endpoint$nam_fin,
-                                   selected = c('LC50', 'EC50'))
-             )
+    menuItem(
+      'Compound',
+      tabName = 'compound',
+      splitLayout(
+        fileInput(
+          inputId = 'file_cas',
+          label = 'Upload CAS',
+          accept = '.csv',
+          placeholder = 'one column .csv'
+        ),
+        actionButton(
+          inputId = 'reset',
+          label = 'Reset',
+          style = 'margin-top:37px'
+        )
+      ),
+      prettyCheckboxGroup(
+        inputId = 'conc_type',
+        label = 'Concentration type',
+        choiceValues = te_stats_l$tes_conc_type$variable,
+        choiceNames = te_stats_l$tes_conc_type$nam_fin,
+        selected = c('A')
+      ),
+      prettyCheckboxGroup(
+        inputId = 'chem_class',
+        label = 'Chemical class',
+        choiceValues = te_stats_l$chem_class$variable,
+        choiceNames = te_stats_l$chem_class$nam_fin,
+        selected = 'cgr_herbicide'
+      )
     ),
-    menuItem('Checks', tabName = 'checks',
-             prettyCheckbox(inputId = 'chck_solub', label = 'Water solubility check'),
-             prettyCheckbox(inputId = 'chck_outlier', label = 'Remove outliers',
-                            value = TRUE)
+    menuItem(
+      'Taxon',
+      tabName = 'taxon',
+      selectInput(
+        inputId = 'tax',
+        label = 'Choose a taxon',
+        choices = c(
+          'Daphniidae',
+          'Chironomidae',
+          'Insecta',
+          'Crustacea',
+          'Annelida',
+          'Platyhelminthes',
+          'Mollusca',
+          'Makro_Inv',
+          'Fish',
+          'Algae',
+          'Bacillariophyceae',
+          'Plants'
+        )
+      ),
+      prettyCheckboxGroup(
+        inputId = 'habitat',
+        label = 'Organism hatbitat',
+        choiceValues = te_stats_l$habitat$variable,
+        choiceNames = te_stats_l$habitat$nam_fin,
+        selected = 'hab_fresh'
+      ),
+      prettyCheckboxGroup(
+        inputId = 'continent',
+        label = 'Continent',
+        choiceValues = te_stats_l$continent$variable,
+        choiceNames = te_stats_l$continent$nam_fin,
+        selected = 'reg_europe'
+      )
+    ),
+    menuItem(
+      'Test',
+      tabName = 'test',
+      splitLayout(
+        numericInput(
+          inputId = 'dur1',
+          label = 'Test durations from',
+          value = 24
+        ),
+        numericInput(
+          inputId = 'dur2',
+          label = 'to (h)',
+          value = 48
+        )
+      ),
+      prettyCheckboxGroup(
+        inputId = 'acch',
+        label = 'TODO! Acute / Chronic',
+        # TODO take choices programatically!
+        choiceValues = c('acute', 'chronic', 'nc'),
+        choiceNames = c('acute', 'chronic', 'not classified')
+      ),
+      splitLayout(
+        prettyCheckboxGroup(
+          inputId = 'effect',
+          label = 'Effect group',
+          choiceValues = te_stats_l$tes_effect$variable,
+          choiceNames = te_stats_l$tes_effect$nam_fin,
+          selected = c('MOR', 'POP', 'GRO', 'ITX')
+        ),
+        prettyCheckboxGroup(
+          inputId = 'endpoint',
+          label = 'Endpoints',
+          choiceValues = te_stats_l$tes_endpoint$variable,
+          choiceNames = te_stats_l$tes_endpoint$nam_fin,
+          selected = c('LC50', 'EC50')
+        )
+      )
+    ),
+    menuItem(
+      'Checks',
+      tabName = 'checks',
+      prettyCheckbox(inputId = 'chck_solub', label = 'Water solubility check'),
+      prettyCheckbox(
+        inputId = 'chck_outlier',
+        label = 'Remove outliers',
+        value = TRUE
+      )
     )
   ),
   sidebarMenu(
     'Aggregation',
     id = 'sidebar_agg',
     icon = icon('list-alt'),
-    menuItem('Aggregation', tabName = 'aggregation',
-             prettyCheckboxGroup(inputId = 'agg', label = 'Aggregate',
-                                 choiceValues = c('min', 'max', 'md', 'mn', 'sd'),
-                                 choiceNames = c('Minimum', 'Maximum', 'Median', 'Mean', 'Standard Deviation'),
-                                 selected = 'md')
-             )
+    menuItem(
+      'Aggregation',
+      tabName = 'aggregation',
+      prettyCheckboxGroup(
+        inputId = 'agg',
+        label = 'Aggregate',
+        choiceValues = c('min', 'max', 'md', 'mn', 'sd'),
+        choiceNames = c('Minimum', 'Maximum', 'Median', 'Mean', 'Standard Deviation'),
+        selected = 'md'
+      )
+    )
   )
 )
 
@@ -100,76 +171,86 @@ sidebar = dashboardSidebar(
 rightsidebar = rightSidebar()
 
 # body --------------------------------------------------------------------
-body = dashboardBody(
-  #setShadow(class = "dropdown-menu"),
+body = dashboardBody(#setShadow(class = "dropdown-menu"),
   #tags$head(includeCSS('style.css')),
   fluidRow(
-    box(title = 'Etox-Base',
-        status = 'success',
-        width = 12,
-        collapsible = TRUE,
-        withMathJax(includeMarkdown('README.md')))
-        # https://stackoverflow.com/questions/33499651/rmarkdown-in-shiny-application
+    box(
+      title = 'Etox-Base',
+      status = 'success',
+      width = 12,
+      collapsible = TRUE,
+      withMathJax(includeMarkdown('README.md'))
+    )
+    # https://stackoverflow.com/questions/33499651/rmarkdown-in-shiny-application
   ),
   fluidRow(
-    box(title = 'Aggregated values',
-        status = 'primary',
-        dataTableOutput(outputId = 'dat'),
-        width = 9, offset = 0),
-    box(title = 'Inputs',
-        status = 'primary',
-        width = 3,
-        prettyCheckboxGroup(inputId = 'comp', label = 'Compound columns',
-                            choiceValues = c('comp_name', 'comp_type'),
-                            choiceNames = c('Compound name', 'Compound type'),
-                            selected = c('cas', 'comp_name')),
-        prettyCheckboxGroup(inputId = 'infocols', label = 'Information columns',
-                            choiceValues = c('info', 'taxa', 'vls', 'n'),
-                            choiceNames = c('info', 'taxa', 'values', 'n'),
-                            selected = 'taxa'))
+    box(
+      title = 'Aggregated values',
+      status = 'primary',
+      dataTableOutput(outputId = 'dat'),
+      width = 9,
+      offset = 0
+    ),
+    box(
+      title = 'Inputs',
+      status = 'primary',
+      width = 3,
+      prettyCheckboxGroup(
+        inputId = 'comp',
+        label = 'Compound columns',
+        choiceValues = c('comp_name', 'comp_type'),
+        choiceNames = c('Compound name', 'Compound type'),
+        selected = c('cas', 'comp_name')
+      ),
+      prettyCheckboxGroup(
+        inputId = 'infocols',
+        label = 'Information columns',
+        choiceValues = c('info', 'taxa', 'vls', 'n'),
+        choiceNames = c('info', 'taxa', 'values', 'n'),
+        selected = 'taxa'
+      )
+    )
   ),
   fluidRow(
-    box(title = 'Plotly Sensitivity',
-        status = 'primary',
-        width = 9,
-        plotlyOutput(outputId = 'plotly_sensitivity')),#,
-                     #height = sprintf('%spx', n_pl * 400))),
-    box(title = 'Inputs',
-        status = 'primary',
-        width = 3,
-        numericInput(inputId = 'cutoff',
-                     label = 'Number of compounds',
-                     value = 25, width = '120px'),
-        prettyRadioButtons(inputId = 'yaxis', label = 'y-axis',
-                           choiceValues = c('casnr', 'comp_name'),
-                           choiceNames = c('CAS', 'Compound name'),
-                           selected = 'casnr', inline = FALSE),
-        prettyRadioButtons(inputId = 'xaxis', label = 'x-axis',
-                           choiceValues = c('limout', 'log'),
-                           choiceNames = c('Limit to x-axis range', 'Log x-axis'),
-                           selected = 'limout', inline = FALSE))
+    box(
+      title = 'Plotly Sensitivity',
+      status = 'primary',
+      width = 9,
+      plotlyOutput(outputId = 'plotly_sensitivity')
+    ),
+    #,
+    #height = sprintf('%spx', n_pl * 400))),
+    box(
+      title = 'Inputs',
+      status = 'primary',
+      width = 3,
+      numericInput(
+        inputId = 'cutoff',
+        label = 'Number of compounds',
+        value = 25,
+        width = '120px'
+      ),
+      prettyRadioButtons(
+        inputId = 'yaxis',
+        label = 'y-axis',
+        choiceValues = c('casnr', 'comp_name'),
+        choiceNames = c('CAS', 'Compound name'),
+        selected = 'casnr',
+        inline = FALSE
+      ),
+      prettyRadioButtons(
+        inputId = 'xaxis',
+        label = 'x-axis',
+        choiceValues = c('limout', 'log'),
+        choiceNames = c('Limit to range', 'Log x-axis'),
+        selected = 'limout',
+        inline = FALSE
+      )
+    )
   )
-  #! REMOVED, TAKES TOO MUCH TIME
-  #,
-  # fluidRow(
-  #   box(title = 'SSD',
-  #       status = 'primary',
-  #       width = 9,
-  #       plotOutput(outputId = 'plot_ssd')),
-  #   box(title = 'Input',
-  #       status = 'primary',
-  #       width = 3)
-  # )
 )
-  
 
 # page --------------------------------------------------------------------
 ui = dashboardPagePlus(header, sidebar, body,
                        title = 'Etox Base',
                        skin = 'purple')
-
-
-
-
-
-
