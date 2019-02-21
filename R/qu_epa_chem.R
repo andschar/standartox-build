1bca8e4ca36872d9cbfcbef7a881e0c26ff4fc7d
andreasLD <andschar@gmail.com>
2019-02-21 18:33:01 +0100

    added (renamed) epa chemicals and taxon scripts

--- R/qu_epa_chem.R
+++ R/qu_epa_chem.R
@@ -3,10 +3,7 @@
 # setup -------------------------------------------------------------------
 source(file.path(src, 'setup.R'))
 
-# data base
-DBetox = readRDS(file.path(cachedir, 'data_base_name_version.rds'))
-
-# (1) query ---------------------------------------------------------------
+# query ---------------------------------------------------------------
 if (online_db) {
   drv = dbDriver("PostgreSQL")
   con = dbConnect(drv, user = DBuser, dbname = DBetox, host = DBhost, port = DBport, password = DBpassword)
@@ -15,15 +12,16 @@ if (online_db) {
                              FROM ecotox.chemicals")
   setDT(cla_che)
   cla_che[ , cas_number := as.character(cas_number) ]
+  cla_che[ , cas := casconv(cas_number) ][ , cas_number := NULL ]
   
   dbDisconnect(con)
   dbUnloadDriver(drv); rm(con, drv)
   
-  saveRDS(cla_che, file.path(cachedir, 'source_epa_classification_chemicals.rds'))
+  saveRDS(cla_che, file.path(cachedir, 'ep_chemicals_source.rds'))
   
 } else {
   
-  cla_che = readRDS(file.path(cachedir, 'source_epa_classification_chemicals.rds'))
+  cla_che = readRDS(file.path(cachedir, 'ep_chemicals_source.rds'))
 }
 
 # classification chemicals ------------------------------------------------
@@ -70,11 +68,17 @@ cla_che[ grep(paste0(pesticide, collapse = '|'), ecotox_group),
 
 # final table -------------------------------------------------------------
 cla_che = cla_che[ , .SD, .SDcols =! 'ecotox_group' ]
-setnames(cla_che, 'cas_number', 'casnr')
-cla_che = cla_che[ , lapply(.SD, as.integer), .SDcols =! 'casnr', casnr ]
+ep_chem_fin = cla_che[ , lapply(.SD, as.integer), .SDcols =! 'cas', cas ]
+
+# writing -----------------------------------------------------------------
+saveRDS(ep_chem_fin, file.path(cachedir, 'ep_chem_fin.rds'))
+
+# log ---------------------------------------------------------------------
+msg = 'EPA chemicals script run'
+log_msg(msg); rm(msg)
 
 # cleaning ----------------------------------------------------------------
-rm()
+clean_workspace()
 
 
 
