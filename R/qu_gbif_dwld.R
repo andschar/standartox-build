02977c7e12366f351895ece19d35859995856347
andreas <andschar@gmail.com>
2019-05-22 17:02:55 +0200

    bug

--- R/qu_gbif_dwld.R
+++ R/qu_gbif_dwld.R
@@ -37,9 +37,9 @@ for (i in seq_along(todo_gbif)) {
   taxon = todo_gbif[i]
   message('GBIF: Querying (', i, '/', length(todo_gbif), '): ', taxon)
   
-  key = name_backbone(taxon)$speciesKey
+  key = try(name_backbone(taxon)$speciesKey)
   
-  if (!is.null(key)) {
+  if (!is.null(key) & !inherits(key, 'try-error')) {
     gbif = tryCatch({
       occ_search(taxonKey = key)
     }, error = function(e) { cat('ERROR: ', conditionMessage(e), '\n'); return(NA) })
