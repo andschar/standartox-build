1cef677ad6325165ed14c1015283d510a25a1ba0
andreas <andschar@gmail.com>
2019-07-15 12:31:28 +0200

    added summary table to NORMAN export

--- R/exp_norman.R
+++ R/exp_norman.R
@@ -11,6 +11,13 @@ export_tbl(schema = 'norman', table = 'data', type = 'csv',
 export_tbl(schema = 'norman', table = 'data2', type = 'csv',
            user = DBuser, host = DBhost, port = DBport, password = DBpassword, dbname = DBetox)
 
+# summary -----------------------------------------------------------------
+summary_nor1 = summary_db_all(schema = 'norman', table = 'data1')
+summary_nor2 = summary_db_all(schema = 'norman', table = 'data2')
+
+fwrite(summary_nor1, file.path(normandir, 'summary_norman1.csv'))
+fwrite(summary_nor2, file.path(normandir, 'summary_norman2.csv'))
+
 # log ---------------------------------------------------------------------
 log_msg('Export: application data exported')
 
