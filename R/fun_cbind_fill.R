7dd0725e256600934f6138983f46412c065f416f
andreas <andschar@gmail.com>
2019-09-02 17:32:48 +0200

    added cbind_fill()

---
+++ R/fun_cbind_fill.R
@@ -0,0 +1,17 @@
+# function to bind data.frame s in a list to a single data.frame
+# https://stackoverflow.com/questions/7962267/cbind-a-dataframe-with-an-empty-dataframe-cbind-fill
+
+cbind_fill <- function(..., dflist = NULL, limit = NULL){
+  nm <- c(list(...), dflist)
+  nm <- lapply(nm, as.matrix)
+  n <- max(sapply(nm, nrow)) 
+  out <- do.call(cbind, lapply(nm, function (x) 
+    rbind(x, matrix(, n-nrow(x), ncol(x)))))
+  out <- data.frame(out)
+  
+  if (!is.null(limit)) {
+    out <- out[1:limit, ]
+  }
+  
+  return(out)
+}
