a5ce54db2007551b9af3456bb13de0cda0c3077c
andreas <andschar@gmail.com>
2019-09-25 18:04:31 +0200

    bug

--- R/cpy_norman.R
+++ R/cpy_norman.R
@@ -4,13 +4,19 @@
 source(file.path(src, 'gn_setup.R'))
 
 # copy --------------------------------------------------------------------
-file.copy(from = normandir,
-          to = cloud,
-          recursive = TRUE,
-          overwrite = TRUE)
-
+if (nodename == 'scharmueller-t460s') {
+  file.copy(from = normandir,
+            to = cloud,
+            recursive = TRUE,
+            overwrite = TRUE)
+  msg = 'Copy: NORMAN export copied to cloud.'
+} else {
+  # TODO install Nextcloud on server for automatic cloud update
+  msg = 'Copy: NORMAN - copy by hand to cloud (TODO) !!!'
+}
+  
 # log ---------------------------------------------------------------------
-log_msg('Copy: NORMAN export copied to cloud.')
+log_msg(msg)
 
 # cleaning ----------------------------------------------------------------
 clean_workspace()
