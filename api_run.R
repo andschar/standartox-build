1809b1f053e71f1af87d8c8aa4091b95b24aea8c
andreas <andschar@gmail.com>
2019-10-02 16:47:06 +0200

    httr requirement

--- api_run.R
+++ api_run.R
@@ -4,9 +4,9 @@ source(file.path(prj, 'setup.R'))
 
 nodename = Sys.info()[4]
 if(nodename == 'uwigis') {
-  host = '139.14.20.252'
+  host = 'http://139.14.20.252'
 } else {
-  host = '127.0.0.1'
+  host = 'http://127.0.0.1'
 }
 
 pr = plumber::plumb('api.R')
